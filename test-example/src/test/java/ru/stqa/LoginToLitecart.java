df14e04922a199f411739660151c30be6f56fe2f
olga148 <82608323+olga148@users.noreply.github.com>
2021-08-18 22:23:35 +0300

    Сценарий для логина в панель администрирования
    панель администрирования учебного приложения

---
+++ test-example/src/test/java/ru/stqa/LoginToLitecart.java
@@ -0,0 +1,37 @@
+package ru.stqa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginToLitecart {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeAll
+    static void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void loginToLitecart() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+}
+
