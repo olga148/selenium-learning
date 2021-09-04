55db44e67eb8a6a806355e018c9c609d7fae1af3
olga148 <82608323+olga148@users.noreply.github.com>
2021-09-04 18:38:46 +0300

    Задание 7
    Провал. минус 8 часов попыток решить эту задачку

--- test-example/src/test/java/ru/stqa/WalkingToMenu.java
+++ test-example/src/test/java/ru/stqa/WalkingToMenu.java
@@ -10,13 +10,16 @@ public class WalkingToMenu extends TestBase {
     public void goToAppearence() {
         driver.findElement(By.xpath("//li[@id='app-']/a/span[@class='name']")).click();
         driver.findElement(By.id("doc-template")).click();
-        Assertions.assertEquals("Template", "Template");
+        driver.findElement(By.tagName("h1"));
         driver.findElement(By.id("doc-logotype")).click();
-        Assertions.assertEquals("Logotype", "Logotype");
+        driver.findElement(By.tagName("h1"));
     }
 
     @Test
     public void goToCatalog() {
+        driver.findElement(By.xpath("//li[2]/a/span[2]")).click();
+        driver.findElement(By.id("doc-catalog")).click();
+        driver.findElement(By.tagName("h1"));
 
     }
 
