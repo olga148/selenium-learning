a8c22b44a0d39bfd5ca94254c2ff29dbe323fa33
olga148 <82608323+olga148@users.noreply.github.com>
2021-09-07 20:08:14 +0300

    Задание 7

--- test-example/src/test/java/ru/stqa/WalkingToMenu.java
+++ test-example/src/test/java/ru/stqa/WalkingToMenu.java
@@ -1,27 +1,27 @@
 package ru.stqa;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class WalkingToMenu extends TestBase {
 
     @Test
-    public void goToAppearence() {
-        driver.findElement(By.xpath("//li[@id='app-']/a/span[@class='name']")).click();
-        driver.findElement(By.id("doc-template")).click();
-        driver.findElement(By.tagName("h1"));
-        driver.findElement(By.id("doc-logotype")).click();
-        driver.findElement(By.tagName("h1"));
+    public void walkThrouhgTheMenu() {
+        List<WebElement> links = driver.findElements(By.id("app-"));
+        List<WebElement> number = driver.findElements(By.className("docs"));
+        int countOfElements = links.size();
+        int numbersOfElements = number.size();
+        for (int i=0; i<countOfElements; i++) {
+            links = driver.findElements(By.id("app-"));
+            links.get(i).click();
+            for (int n=0; n<numbersOfElements; n++) {
+                number = driver.findElements(By.className("docs"));
+                number.get(n).click();
+                driver.findElement(By.tagName("h1"));
+            }
+        }
     }
-
-    @Test
-    public void goToCatalog() {
-        driver.findElement(By.xpath("//li[2]/a/span[2]")).click();
-        driver.findElement(By.id("doc-catalog")).click();
-        driver.findElement(By.tagName("h1"));
-
-    }
-
-
 }
